b9876fc8dc145d1d1a4bd9b26c99a47ae074bc72
RaulVs23 <131603951+RaulVs23@users.noreply.github.com>
2024-04-06 13:20:50 -0600

    “Se agrega CRUD para Materias”

---
+++ ADSProject/Interfaces/IProfesor.cs
@@ -0,0 +1,17 @@
+﻿using ADSProject.Models;
+
+namespace ADSProject.Interfaces
+{
+    public interface IProfesor
+    {
+        public int AgregarProfesor(Profesor profesor);
+
+        public int ActualizarProfesor(int IdProfesor, Profesor profesor);
+
+        public bool EliminarProfesor(int IdProfesor);
+
+        public List<Profesor> ObtenertodasLosProfesores();
+
+        public Profesor ObtenerProfesoresPorId(int IdProfesor);
+    }
+}
---
+++ ADSProject/Models/Materia.cs
@@ -0,0 +1,9 @@
+﻿namespace ADSProject.Models
+{
+    public class Materia
+    {
+        public int IdMateria { get; set; }
+
+        public string Nombremateria { get; set; }
+    }
+}
