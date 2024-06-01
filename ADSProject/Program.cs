b32f0e8cc62ddc395a4357ae320f9a20c4280213
Raul <vc21i04001@usonsonate.edu.sv>
2024-06-01 10:01:01 -0600

    GUIA8

--- ADSProject/Program.cs
+++ ADSProject/Program.cs
@@ -1,42 +1,39 @@
 using ADSProject.DB;
 using ADSProject.Interfaces;
-using ADSProject.Models;
 using ADSProject.Repositories;
 using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
-
-//Configurando DBContext
-builder.Services.AddDbContext<ApplicationDbContext>(opciones => opciones.UseSqlServer("name = DefaultConnection"));
-
+// Configurando DBContext
+builder.Services.AddDbContext<ApplicationDbContext>(opciones =>
+opciones.UseSqlServer("name=DefaultConnection"));
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at
+https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+//Configuracion inyeccion de dependencias
 builder.Services.AddScoped<IEstudiante, EstudianteRepository>();
-
 builder.Services.AddScoped<ICarrera, CarreraRepository>();
-
 builder.Services.AddScoped<IMateria, MateriaRepository>();
-
 builder.Services.AddScoped<IProfesor, ProfesorRepository>();
-
 builder.Services.AddScoped<IGrupo, GrupoRepository>();
-
+// Configurando CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(configuration =>
+    {
+        configuration.WithOrigins(builder.Configuration["allowedOrigins"]!).AllowAnyMethod().AllowAnyHeader();
+    });
+});
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
